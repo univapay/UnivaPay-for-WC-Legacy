5c052d815fafe76e58a0a050f75f7a47e10f8e30
Yui Maruyama <16yuim@gmail.com>
2022-05-30 13:10:03 +0900

    fix url

--- UnivaPay-for-WooCommerce-Legacy.php
+++ UnivaPay-for-WooCommerce-Legacy.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://univapay.com
  * Description: UnivaPayを使用して店舗でクレジットカード決済が可能です。
  * Author: UnivaPay
- * Author URI: https://univapay.com
+ * Author URI: https://univapay.com/service/
  * Version: 0.2.9
  *
  /*
